74e1037ee9803c2795b46e658a7fa8b134b99f29
Blaine Lewis <blaine.lewis.1@outlook.com>
2016-07-21 18:30:57 +1000

    Refactored, added controls and made it full screen. Made it pause on unfocus

--- scripts/index.js
+++ scripts/index.js
@@ -1,167 +1,201 @@
-//TODO: add sliders to adjust things
+(function() {
+    var config = {};
+    config.arm_length = {value : 150, min : 1, max : 500};
+    config.gravity = {value : 0.001, min : 0.0001, max : 0.01};
+    config.friction = {value : 0.995, min : 0.9, max : 1};
+    config.cart_speed = {value : 0.5, min : 0.1, max : 1};
+
+    var control_box = document.createElement("div");
+    control_box.classList.add("controls");
+    for(var prop in config) {
+        if(config.hasOwnProperty(prop)) {
+            var elem = document.createElement("input");
+            elem.type = "range";
+            elem.id = prop;
+            elem.value = config[prop].value;
+            elem.min = config[prop].min;
+            elem.max = config[prop].max;
+            elem.step = (config[prop].max - config[prop].min) / 20;
+            elem.addEventListener("input", update_value);
+
+            var label = document.createElement("label");
+            label.innerHTML = prop;
+
+            control_box.appendChild(label);
+            control_box.appendChild(elem);
+        }
+    }
+
+    document.body.appendChild(control_box);
+
+    function update_value(e) {
+        config[e.target.id].value = e.target.value;
+    }
 
 
-var ARM_LENGTH = 150;
-var STAGE_WIDTH = 800;
-var STAGE_HEIGHT = ARM_LENGTH * 2 + 100;
-var GRAVITY = 0.001;
+    var EDGE_OFFSET = 10;
 
-function drawScene(context) {
-  context.lineWidth = 5;
-  context.strokeStyle = "#AAAAAA";
-  context.lineCap = "round";
-
-  context.beginPath();
-  context.moveTo(0, STAGE_HEIGHT);
-  context.lineTo(STAGE_WIDTH, STAGE_HEIGHT);
-
-  context.moveTo(0,STAGE_HEIGHT);
-  context.lineTo(0, STAGE_HEIGHT - 20);
-
-  context.moveTo(STAGE_WIDTH, STAGE_HEIGHT);
-  context.lineTo(STAGE_WIDTH, STAGE_HEIGHT - 20);
-
-  context.stroke();
-}
-
-//0 degrees is actually Math.PI/2 to make the physics easier.
-function Pendulum(number_of_axis) {
-  this.x = STAGE_WIDTH / 2;
-  this.axis = [];
-  this.y = STAGE_HEIGHT;
-  this.speed = 0.5;
-  this.height = 10;
-  this.width = 40;
-
-  for(var i = 0; i < number_of_axis; i++) {
-    this.axis.push({theta : Math.PI / 2, thetaVelocity : 0, v : 0, l : ARM_LENGTH});
-  }
-}
-
-Pendulum.prototype.draw = function(context) {
-  context.lineWidth = 5;
-  context.strokeStyle = "#444444";
-  context.lineCap = "round";
-  context.fillStyle = "#444444";
-
-  context.beginPath();
-  context.fillRect(this.x - this.width / 2, STAGE_HEIGHT - this.height, this.width, this.height);
-
-  context.fillStyle = "#AAAAAA";
-
-  context.beginPath();
-  context.arc(this.x - (this.width / 4), this.y - (this.height / 2), (this.height / 2), 0, Math.PI * 2);
-  context.fill();
-
-  context.beginPath();
-  context.arc(this.x + (this.width / 4), this.y - (this.height / 2), (this.height / 2), 0, Math.PI * 2);
-  context.fill();
-
-  context.beginPath();
-  context.moveTo(this.x, this.y);
-
-  var curX = this.x;
-  var curY = STAGE_HEIGHT;
-
-  for(var i = 0; i < this.axis.length; i++) {
-    curX += Math.cos(this.axis[i].theta + 3 * Math.PI / 2) * ARM_LENGTH;
-    curY -= Math.sin(this.axis[i].theta + 3 * Math.PI / 2 ) * ARM_LENGTH;
-
-    // curX += Math.cos(this.axis[i].theta) * ARM_LENGTH;
-    // curY -= Math.sin(this.axis[i].theta) * ARM_LENGTH;
-
-    context.lineTo(curX, curY);
-  }
-
-  context.stroke();
-};
-
-Pendulum.prototype.move = function(deltaTime, leftKeyDown, rightKeyDown) {
-  if(leftKeyDown && this.x > this.width - this.speed * deltaTime) {
-    this.x -= this.speed * deltaTime;
-    //TODO: this needs to be acceleration so not addition
-    this.axis[0].thetaVelocity += 0.0001;
-  }
-
-  if(rightKeyDown && this.x < STAGE_WIDTH - this.width + this.speed * deltaTime) {
-    this.x += this.speed * deltaTime;
-    this.axis[0].thetaVelocity -= 0.0001;
-  }
-};
-
-Pendulum.prototype.applyPhysics = function(deltaTime) {
-  for(var i = 0; i < this.axis.length; i++) {
-    var axis = this.axis[i];
-    //This is the second derivative of theta, which is actually the "acceleration of theta"
-
-    //We take axis.theta * time  and add that to thetaVelocity, then take thetaVelocity * time and add that to the displacement. BAMMM
-    //axis.theta += -GRAVITY / ARM_LENGTH * Math.sin(axis.theta);
-
-    axis.thetaVelocity += -GRAVITY / ARM_LENGTH * Math.sin(axis.theta) * deltaTime;
-    axis.thetaVelocity *= 0.995;
-    axis.theta += axis.thetaVelocity * deltaTime;
+    function draw_scene(context, canvas) {
+      context.lineWidth = 5;
+      context.strokeStyle = "#AAAAAA";
+      context.lineCap = "round";
 
 
+      context.beginPath();
+      context.moveTo(EDGE_OFFSET, canvas.height / 2);
+      context.lineTo(canvas.width - EDGE_OFFSET, canvas.height / 2);
 
-    //axis.theta = axis.theta % (Math.PI * 2);
-  }
-};
+      context.moveTo(EDGE_OFFSET, canvas.height / 2);
+      context.lineTo(EDGE_OFFSET, (canvas.height / 2) - 20);
 
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
+      context.moveTo(canvas.width - EDGE_OFFSET, canvas.height / 2);
+      context.lineTo(canvas.width - EDGE_OFFSET, (canvas.height / 2) - 20);
 
-canvas.width = STAGE_WIDTH;
-canvas.height = STAGE_HEIGHT * 2;
+      context.stroke();
+    }
 
-var pendulum = new Pendulum(1);
+    //0 degrees is actually Math.PI/2 to make the physics easier.
+    function Pendulum(number_of_axis) {
+      this.x = canvas.width / 2;
+      this.axis = [];
+      this.y = canvas.height / 2;
+      this.height = 10;
+      this.width = 40;
 
-var previousTime;
-var playing = true;
-var leftKeyDown = false;
-var rightKeyDown = false;
+      for(var i = 0; i < number_of_axis; i++) {
+        this.axis.push({theta : Math.PI / 2, thetaVelocity : 0, v : 0, l : config.arm_length.value});
+      }
+    }
 
-function step(t) {
-  if(playing) {
-    var deltaTime = previousTime === undefined ? 0 : t - previousTime;
+    Pendulum.prototype.draw = function(context) {
+      context.lineWidth = 5;
+      context.strokeStyle = "#444444";
+      context.lineCap = "round";
+      context.fillStyle = "#444444";
 
-    pendulum.move(deltaTime, leftKeyDown, rightKeyDown);
-    pendulum.applyPhysics(deltaTime);
-  }
+      context.beginPath();
+      context.fillRect(this.x - this.width / 2, this.y - this.height, this.width, this.height);
 
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  pendulum.draw(context);
-  drawScene(context);
+      context.fillStyle = "#AAAAAA";
 
-  previousTime = t;
-  window.requestAnimationFrame(step);
-}
+      context.beginPath();
+      context.arc(this.x - (this.width / 4), this.y - (this.height / 2), (this.height / 2), 0, Math.PI * 2);
+      context.fill();
 
-function handleMoveKeys(e) {
-  var val;
+      context.beginPath();
+      context.arc(this.x + (this.width / 4), this.y - (this.height / 2), (this.height / 2), 0, Math.PI * 2);
+      context.fill();
 
-  if(e.type === "keydown") {
-    val = true;
-  } else if(e.type === "keyup"){
-    val = false;
-  } else {
-    return;
-  }
+      context.beginPath();
+      context.moveTo(this.x, this.y);
 
-  if (e.keyCode === 65) {
-    leftKeyDown = val;
-  } else if (e.keyCode === 68) {
-    rightKeyDown = val;
-  }
-}
+      var curX = this.x;
+      var curY = this.y;
 
-document.addEventListener("keydown", handleMoveKeys);
-document.addEventListener("keyup", handleMoveKeys);
+      for(var i = 0; i < this.axis.length; i++) {
+        curX += Math.cos(this.axis[i].theta + 3 * Math.PI / 2) * config.arm_length.value;
+        curY -= Math.sin(this.axis[i].theta + 3 * Math.PI / 2 ) * config.arm_length.value;
 
-document.addEventListener("keypress", function(e) {
-  if(e.keyCode === 32) {
-    playing = !playing;
-    e.preventDefault();
-  }
-});
+        // curX += Math.cos(this.axis[i].theta) * config.arm_length.value;
+        // curY -= Math.sin(this.axis[i].theta) * config.arm_length.value;
 
-step();
+        context.lineTo(curX, curY);
+      }
+
+      context.stroke();
+    };
+
+    Pendulum.prototype.move = function(deltaTime, direction, canvas) {
+        var move_amount = config.cart_speed.value * deltaTime * direction;
+
+      if(direction === -1 && this.x + move_amount - this.width > EDGE_OFFSET ) {
+        this.x += move_amount;
+        //TODO: this needs to be acceleration so not addition
+        this.axis[0].thetaVelocity -= move_amount / 100000;
+      }
+
+      if(direction === 1 && this.x - move_amount + this.width < canvas.width - EDGE_OFFSET) {
+        this.x += move_amount;
+        this.axis[0].thetaVelocity -= move_amount / 100000;
+      }
+    };
+
+    Pendulum.prototype.applyPhysics = function(deltaTime) {
+      for(var i = 0; i < this.axis.length; i++) {
+        var axis = this.axis[i];
+        //This is the second derivative of theta, which is actually the "acceleration of theta"
+
+        //We take axis.theta * time  and add that to thetaVelocity, then take thetaVelocity * time and add that to the displacement. BAMMM
+        //axis.theta += -GRAVITY / config.arm_length.value * Math.sin(axis.theta);
+
+        axis.thetaVelocity += -config.gravity.value / config.arm_length.value * Math.sin(axis.theta) * deltaTime;
+        //TODO: add deltatime to make rfiction dependent on time
+        axis.thetaVelocity *= config.friction.value;
+        axis.theta += axis.thetaVelocity * deltaTime;
+
+
+
+        //axis.theta = axis.theta % (Math.PI * 2);
+      }
+    };
+
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    var pendulum = new Pendulum(1);
+
+    var previous_time;
+    var left_key_down = false;
+    var right_key_down = false;
+    var was_hidden;
+
+    function step(t) {
+        var delta_time = previous_time === undefined ? 0 : t - previous_time;
+
+        if(was_hidden) {
+            delta_time = 0;
+            was_hidden = false;
+        }
+
+        var direction = 0;
+        direction += left_key_down ? -1 : 0;
+        direction += right_key_down ? 1 : 0;
+
+        if(direction){
+            pendulum.move(delta_time, direction, canvas);
+        }
+
+        pendulum.applyPhysics(delta_time);
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        pendulum.draw(context);
+        draw_scene(context, canvas);
+
+        previous_time = t;
+        window.requestAnimationFrame(step);
+    }
+
+    function handle_move_keys(e) {
+      var val = e.type === "keydown";
+
+      if (e.keyCode === 65) {
+        left_key_down = val;
+      } else if (e.keyCode === 68) {
+        right_key_down = val;
+      }
+    }
+
+    document.addEventListener("keydown", handle_move_keys);
+    document.addEventListener("keyup", handle_move_keys);
+
+    document.addEventListener("visibilitychange", function (e) {
+        if(document.hidden) {
+            was_hidden = true;
+        }
+    });
+
+    step();
+})();
